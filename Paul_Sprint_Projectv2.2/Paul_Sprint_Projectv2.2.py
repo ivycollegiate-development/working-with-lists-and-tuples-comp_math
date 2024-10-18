a126580aa7288fd2c0092e7c516b57fb1eb80248
PaulTJJ <pjones25_student@ivycollegiate.org>
2024-10-18 05:58:30 +0000

    changed file names

--- Paul_Sprint_Projectv2.2/Paul_Sprint_Projectv2.2.py
+++ Paul_Sprint_Projectv2.2/Paul_Sprint_Projectv2.2.py
@@ -10,7 +10,7 @@ print (fruits)
 ##3
 
 x.numbers = [1, 2, 3]
-y.y.numbers = [2, 4, 6]
+y.numbers = [2, 4, 6]
 from pylab import plot, show
-plot (x_numbers, y_numbers)
+plot (x.numbers, y.numbers)
 show ()
